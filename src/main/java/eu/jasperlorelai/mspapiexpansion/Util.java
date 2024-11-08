98cf223fc79f19ff8e19dd23c0a35254871dd29e
JasperLorelai <wolf.haci12@gmail.com>
2024-11-08 23:25:23 +0100

    ci: Fix build and change package

---
+++ src/main/java/eu/jasperlorelai/mspapiexpansion/Util.java
@@ -0,0 +1,26 @@
+package eu.jasperlorelai.mspapiexpansion;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Util {
+
+	public static String setPrecision(String str, String precision) {
+		if (precision == null) return str;
+		// Return value if value isn't a floating point - can't be scaled.
+		try {
+			float floatValue = Float.parseFloat(str);
+			// Return value if precision isn't a floating point.
+			int toScale = Integer.parseInt(precision);
+			// Return the scaled value.
+			return BigDecimal.valueOf(floatValue).setScale(toScale, RoundingMode.HALF_UP).toString();
+		} catch (NumberFormatException | NullPointerException ignored) {
+			return str;
+		}
+	}
+
+	public static String colorise(String string) {
+		return com.nisovin.magicspells.util.Util.getLegacyFromMiniMessage(string);
+	}
+
+}
