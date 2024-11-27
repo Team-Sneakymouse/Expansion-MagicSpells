b1f7a6772a99cc52ac48cd4ff4d64a0b52eca7af
JasperLorelai <wolf.haci12@gmail.com>
2024-11-27 20:24:43 +0100

    chore: Bump version

--- src/main/java/eu/jasperlorelai/mspapiexpansion/MagicSpellsPAPIExtension.java
+++ src/main/java/eu/jasperlorelai/mspapiexpansion/MagicSpellsPAPIExtension.java
@@ -31,7 +31,7 @@ public class MagicSpellsPAPIExtension extends PlaceholderExpansion {
 	private static final String IDENTIFIER = "ms";
 	private static final String PLUGIN = "MagicSpells";
 	private static final String NAME = "magicspells";
-	private static final String VERSION = "2.0.0";
+	private static final String VERSION = "2.0.1";
 
 	private MagicSpells plugin;
 
